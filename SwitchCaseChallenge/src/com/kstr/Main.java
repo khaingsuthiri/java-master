6cab92e978a4c0785e6f2128ee40a4c1a87c841e
Khaing Su Thiri <khaingsuthiri@gmail.com>
2022-10-19 11:37:58 +0630

    [Added] exercises for method overloading

---
+++ SwitchCaseChallenge/src/com/kstr/Main.java
@@ -0,0 +1,33 @@
+package com.kstr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char letter = 'F';
+	    switch (letter) {
+            case 'A':
+                System.out.println("Found A");
+                break;
+
+            case 'B':
+                System.out.println("Found B");
+                break;
+
+            case 'C':
+                System.out.println("Found C");
+                break;
+
+            case 'D':
+                System.out.println("Found D");
+                break;
+
+            case 'E':
+                System.out.println("Found E");
+                break;
+
+            default:
+                System.out.println("Not found");
+                break;
+        }
+    }
+}
